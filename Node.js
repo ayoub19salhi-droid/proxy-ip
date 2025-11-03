0078edf65e517e9ede0be4c825ac8258f7e04f75
ayoub19salhi-droid <ayoub19salhi@gmail.com>
2025-11-03 23:37:03 +0100

    Add headers configuration for HTTP requests

---
+++ Node.js
@@ -0,0 +1,8 @@
+headers: {
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0",
+  "Referer": "https://www.google.com/",
+  "Origin": "https://www.google.com",
+  "Accept": "*/*",
+  "Connection": "keep-alive",
+  "Accept-Encoding": "identity"
+},
